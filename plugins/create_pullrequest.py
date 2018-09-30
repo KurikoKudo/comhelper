0e3245154351adbf0dd1f6d496a172857a75b684
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-09-30 17:02:41 +0900

    finish create PullRequest

--- plugins/create_pullrequest.py
+++ plugins/create_pullrequest.py
@@ -4,10 +4,12 @@ import subprocess
 
 
 def create_pullrequest(pr_title):
-    git_cmd = "hub pull-request -m " + pr_title
-    git_cmd_return = subprocess.call(git_cmd.split())
 
-    if git_cmd_return != 0:
+    git_cmd = "hub pull-request -m " + pr_title
+
+    try:
+        git_cmd_return = subprocess.check_output(git_cmd.split())
+    except subprocess.CalledProcessError as e:
         return 'hub pull-request -m の実行でエラーが発生しました。'
-    else:
-        return git_cmd_return
+
+    return git_cmd_return
