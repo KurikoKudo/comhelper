cba407e442d3bbfbad1220f2665b367589e28559
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-10-03 16:05:47 +0900

    テストの議論を開始して comhelper: 議論を開始してください！ kuriko: あいうえお kuriko: かきくけこ pooh: さしすせそ pooh: 議論を終了して

--- plugins/all_response.py
+++ plugins/all_response.py
@@ -4,8 +4,9 @@ from slackbot.bot import respond_to
 
 from .download import download
 from .create_issue import create_issue
-from .discussion import discussion
+from .discussion import discussion, loop, push_discussion
 from .create_pullrequest import create_pullrequest
+from .check_users import check_all_users_by_slack
 
 
 @respond_to('を見せて')
@@ -14,7 +15,6 @@ def mention_func(message):
     成果物ダウンロードコマンド用メソッド
     """
     doc_name = message.body['text'].rstrip('を見せて')
-
     download(message.channel, doc_name)
 
     message.send(doc_name + 'を投稿したよ')
@@ -42,18 +42,32 @@ def mention_func(message):
     message.send(pr_link)
 
 
+@respond_to('ユーザー情報を確認して')
+def mention_func(message):
+    """
+    ユーザー情報確認コマンド用メソッド
+    """
+    users = check_all_users_by_slack()
+    message.send(users)
+    message.send('上記のメンバーでユーザー情報を更新したよ！')
+
+
 @respond_to('の議論を開始')
 def mention_func(message):
     """
     議論コマンド用メソッド
     """
-    message.send('議論を開始してください')
-    # TODO: 議論が開始されていない時はエラー
-    # TODO: echo 'issue番号　任意の議論タイトル' > commit.txt
-
     discussion_title = message.body['text'].rstrip('の議論を開始')
+    discussion_process_ready = discussion(discussion_title)
 
-    discussion(discussion_title)
+    if discussion_process_ready:
+        message.send('議論を開始してください！')
+        loop_return = loop()
 
-    message.send('議論を終了するよ')
-    # TODO: git commit --allow-empty -F commit.txt
+        message.send(loop_return)
+
+        push_status = push_discussion()
+        message.send(push_status)
+
+    else:
+        message.send('echo > commit.txt の実行でエラーが発生しました。')
--- plugins/discussion.py
+++ plugins/discussion.py
@@ -2,23 +2,78 @@
 
 import subprocess
 import time
+import shlex
+
 from slackbot.slackclient import SlackClient
-from slackbot.dispatcher import MessageDispatcher
+from slackbot_settings import API_TOKEN
+from .check_users import get_user_dict
 
 
 def discussion(discussion_title):
-    cmd = 'echo ' + discussion_title + ' > commit.txt'
+
+    cmd = []
+    cmd.append('echo "' + discussion_title + '" > commit.txt')
 
     try:
-        cmd_return = subprocess.check_call(cmd.split())
-    except subprocess.CalledProcessError as e:
-        return 'echo > commit.txt の実行でエラーが発生しました。'
+        cmd_return = subprocess.run(cmd, shell=True)
+    except subprocess.SubprocessError as e:
+        print("[Error] first echo command")
+        return False
 
-        # return cmd_return
+    return True
 
-        while True:
-            events = SlackClient.rtm_read()
-            time.sleep(1)
+
+def loop():
+
+    client = SlackClient(API_TOKEN)
+    user_dict = get_user_dict()
+
+    while True:
+        events = SlackClient.rtm_read(client)
+
+        for event in events:
+            event_type = event.get('type')
+
+            if event_type == 'message':
+                message_text = event.get('text')
+
+                if message_text == "議論を終了":
+                    return '議論を終了しました！'
+                else:
+                    send_user_id = event.get('user')
+                    send_user_name = user_dict[send_user_id]
+                    add_commit_message = []
+                    add_commit_message.append('echo "' + send_user_name + ': ' + message_text + '" >> commit.txt')
+                    try:
+                        cmd_return = subprocess.run(add_commit_message, shell=True)
+                    except subprocess.SubprocessError as e:
+                        return 'echo >> commit.txt の実行でエラーが発生しました。'
+
+        time.sleep(1)
+
+
+def push_discussion():
+    git_add_cmd = 'git add --all'
+    try:
+        git_cmd_return = subprocess.run(shlex.split(git_add_cmd))
+    except subprocess.CalledProcessError:
+        return 'add コマンドでエラーが起きました'
+
+    git_commit_cmd = 'git commit --allow-empty -F commit.txt'
+    try:
+        git_cmd_return = subprocess.run(shlex.split(git_commit_cmd))
+    except subprocess.CalledProcessError:
+        return 'commit コマンドでエラーが起きました'
+
+    git_push_cmd = 'git push origin comhelper'
+    try:
+        git_cmd_return = subprocess.run(shlex.split(git_push_cmd))
+    except subprocess.CalledProcessError:
+        return 'push コマンドでエラーが起きました'
+
+    return 'comhelper ブランチへの push が完了しました。'
+
+
 
 
 
